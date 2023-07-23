9a3223c7b0527f32dc6ae019b5386ec8fdb95732
Emmanuel Oyervides <33361963+emmy0021@users.noreply.github.com>
2023-07-23 00:28:56 -0400

    username already exists

--- popUpForm.js
+++ popUpForm.js
@@ -1,13 +1,15 @@
   // Add an event listener to the input fields
   const nameInput = document.getElementById("nameInput");
   const groupIDInput = document.getElementById("groupIDInput");
-  
+  const errorMessage = document.getElementById("errorMessage");
+  const popupForm = document.getElementById("popupForm");
+
   function handleInput(event) {
     const input = event.target;
     const label = input.previousElementSibling;
     
     if (input.value.trim() === "") {
-      label.style.top = "50%";
+      label.style.top = "20px";
       label.style.fontSize = "16px";
       label.style.color = "#888";
       label.style.backgroundColor = "";
@@ -33,8 +35,6 @@ function popUpForm() {
     // Show the pop-up form with animation
     popupForm.style.display = "block";
     popupForm.style.animation = "fadeIn 0.5s";
-    
-    const groupIDInput = document.getElementById("groupIDInput");
     groupIDInput.value = groupId;    
     groupIDInput.dispatchEvent(new Event('input'));
 
@@ -54,3 +54,47 @@ function popUpForm() {
 nameInput.addEventListener("input", handleInput);
 groupIDInput.addEventListener("input", handleInput);
 
+function showErrorMessage() {
+  errorMessage.style.display = "block";
+  nameInput.style.borderColor = "red";
+  nameInput.style.animation = "shake 0.5s";
+  popupForm.style.height = "262px";
+  setTimeout(() => {
+    nameInput.style.animation = "";
+  }, 3000);
+}
+
+function hideErrorMessage() {
+  errorMessage.style.display = "none";
+  nameInput.style.borderColor = ""; // Remove the red border color
+  popupForm.style.height = "240px"; // Remove the height set for the pop-up form
+}
+
+function hideCreateButton() {
+  const createButton = document.getElementById("createButton");
+  createButton.style.visibility = "hidden";
+  createButton.style.pointerEvents = "none";
+  groupIDInput.setAttribute("readonly", "readonly");
+  const joinButton = document.querySelector(".join-button");
+  joinButton.onclick = joinGroup;
+}
+
+function joinGroup() {
+  const nameInput = document.getElementById("nameInput");
+  const userName = nameInput.value;
+  console.log("this is calleeeeeeeeeed");
+  setUserName(userName);
+  setTimeout(() => {
+    requestScoresUpdate(groupId);
+    }, 1000);
+    closePopUpForm();
+}
+
+
+function unhideCreateButton() {
+  const createButton = document.getElementById("createButton");
+  createButton.style.visibility = "visible";
+  createButton.style.pointerEvents = "auto";
+  groupIDInput.removeAttribute("readonly");
+  joinButton.onclick = initWebSocket;
+}
